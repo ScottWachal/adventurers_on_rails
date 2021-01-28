ee3eb71fbb14126c7a9e65426ab7fa34a3785c02
Scott Wachal <scott.wachal@gmail.com>
2021-01-28 15:12:10 -0600

    Modified Routes for Adventurers and linked adventurers with items on views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   resources :items
+  resources :adventurers, only: %i[index show]
+
+  root to: "adventurers#index"
+
   # DSL - Domain Specific Language
   # all the routing syntax
-  get "/adventurers", to: "adventurers#index", as: "adventurers" # adventurers_path
-  get "/adventurers/:id", to: "adventurers#show", as: "adventurer", constraints: { id: /\d+/ } # adventurer_path
+  # get "/adventurers", to: "adventurers#index", as: "adventurers" # adventurers_path
+  # get "/adventurers/:id", to: "adventurers#show", as: "adventurer", constraints: { id: /\d+/ } # adventurer_path
 end
